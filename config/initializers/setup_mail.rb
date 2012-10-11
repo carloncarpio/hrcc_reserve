d61401627926af657240836c14210085f1df483e
carlo n carpio <carloncarpio@yahoo.com>
2012-10-11 15:03:23 +0800

    commit

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
 	:address			=> "smtp.gmail.com",
 	:port				=> 587,
-	:domain				=> "http://www.gmail.com",
-	:user_name			=> "ccarpio2818@gmail.com",
-	:password			=> "aaron1618",
+	:domain				=> "http://www.yahoo.com",
+	:user_name			=> "carloncarpio@yahoo.com",
+	:password			=> "aaron16",
 	:enable_starttls_auto	=> true
 }
